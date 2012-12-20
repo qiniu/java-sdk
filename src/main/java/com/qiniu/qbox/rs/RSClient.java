1bdf18cab73a3ef2b76a601ce1d424f6eff90a7f
wangjinlei <wangjinlei4ustc@gmail.com>
2012-12-20 18:40:41 +0800

    upload with token

--- src/main/java/com/qiniu/qbox/rs/RSClient.java
+++ src/main/java/com/qiniu/qbox/rs/RSClient.java
@@ -13,6 +13,7 @@ import org.apache.http.entity.mime.content.StringBody;
 import org.apache.http.impl.client.DefaultHttpClient;
 import org.apache.http.util.EntityUtils;
 
+import com.qiniu.qbox.Config;
 import com.qiniu.qbox.auth.CallRet;
 import com.qiniu.qbox.auth.Client;
 import com.qiniu.qbox.up.BlockProgress;
@@ -23,11 +24,64 @@ import com.qiniu.qbox.up.UpService;
 
 public class RSClient {
 	
+	public static PutFileRet putFileWithToken(String upToken, String bucketName,
+			String key, String localFile, String mimeType, String customMeta,
+			Object callbackParam, String rotate) throws Exception {
+
+		File file = new File(localFile);
+		if (!file.exists() || !file.canRead()) {
+			return new PutFileRet(new CallRet(400, new Exception(
+					"File does not exist or not readable.")));
+		}
+
+		if (mimeType == null || mimeType.length() == 0) {
+			mimeType = "application/octet-stream";
+		}
+
+		String entryURI = bucketName + ":" + key;
+		String action = "/rs-put/" + Client.urlsafeEncode(entryURI)
+				+ "/mimeType/" + Client.urlsafeEncode(mimeType);
+
+		if (customMeta != null && customMeta.length() != 0) {
+			action += "/meta/" + Client.urlsafeEncode(customMeta);
+		}
+
+		if (rotate != null && rotate.length() != 0) {
+			action += "/rotate/" + rotate;
+		}
+
+		MultipartEntity requestEntity = new MultipartEntity();
+		requestEntity.addPart("auth", new StringBody(upToken));
+		requestEntity.addPart("action", new StringBody(action));
+		FileBody fileBody = new FileBody(new File(localFile));
+		requestEntity.addPart("file", fileBody);
+
+		if (callbackParam != null) {
+			String callbackParam1 = Client.encodeParams(callbackParam);
+			if (callbackParam1 != null) {
+				requestEntity.addPart("params", new StringBody(callbackParam1));
+			}
+		}
+		HttpPost postMethod = new HttpPost(Config.UP_HOST + "/upload");
+		postMethod.setEntity(requestEntity);
+
+		DefaultHttpClient client = new DefaultHttpClient();
+		try {
+			HttpResponse response = client.execute(postMethod);
+			return handleResult(response);
+		} catch (Exception e) {
+			e.printStackTrace();
+			return new PutFileRet(new CallRet(400, e));
+		} finally {
+			client.getConnectionManager().shutdown();
+		}
+	}
 	/**
 	 * func PutFile(url, bucketName, key, mimeType, localFile, customMeta, callbackParams string)
 	 * 匿名上传一个文件(上传用的临时 url 通过 $rs->PutAuth 得到)
 	 * @throws Exception 
 	 */
+	@Deprecated
 	public static PutFileRet putFile(
 		String url, String bucketName, String key, String mimeType, String localFile,
 		String customMeta, Object callbackParams1) throws Exception {
