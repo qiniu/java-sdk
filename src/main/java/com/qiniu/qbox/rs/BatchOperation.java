3fa54cf0d1945b0602aec99270664b8605d641c8
wangjinlei <wangjinlei4ustc@gmail.com>
2013-02-16 12:52:23 +0800

    f

---
+++ src/main/java/com/qiniu/qbox/rs/BatchOperation.java
@@ -0,0 +1,15 @@
+package com.qiniu.qbox.rs;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class BatchOperation {
+
+	public List<String> uriList = new ArrayList<String>();
+
+	
+	
+	public void execute() {
+		
+	}
+}
