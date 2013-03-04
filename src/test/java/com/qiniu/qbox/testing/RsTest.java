4104ea791cacd6e8a08a23e165a6ce31e5f7b0c8
wangjinlei <wangjinlei4ustc@gmail.com>
2013-03-04 13:07:50 +0800

    add unit test case

--- src/test/java/com/qiniu/qbox/testing/RsTest.java
+++ src/test/java/com/qiniu/qbox/testing/RsTest.java
@@ -5,6 +5,7 @@ import junit.framework.TestCase;
 import com.qiniu.qbox.Config;
 import com.qiniu.qbox.auth.CallRet;
 import com.qiniu.qbox.auth.DigestAuthClient;
+import com.qiniu.qbox.rs.BucketsRet;
 import com.qiniu.qbox.rs.DeleteRet;
 import com.qiniu.qbox.rs.DropRet;
 import com.qiniu.qbox.rs.GetRet;
@@ -22,6 +23,10 @@ public class RsTest extends TestCase {
 	public final String expectedHash = "FmDZwqadA4-ib_15hYfQpb7UXUYR" ;
 	public RSService rs ;
 	public String bucketName ;
+	
+	// use fixed buckets
+	public final String srcBucket = "junit_bucket_src";
+	public final String destBucket = "junit_bucket_dest";
 	
 	public void setUp() {
 		
@@ -107,6 +112,111 @@ public class RsTest extends TestCase {
 		RSService rs = new RSService(conn, delBucketName) ;
 		DropRet dropRet = rs.drop() ;
 		assertTrue(dropRet.ok()) ;
+	}
+	
+	public void testMove() throws Exception {
+		
+		String srcKey = "src-key-move";
+		String destKey = "dest-key-move";
+		String entryUriSrc = srcBucket + ":" + srcKey;
+		String entryUriDest = destBucket + ":" + destKey;
+		DigestAuthClient conn = new DigestAuthClient();
+		rs = new RSService(conn, srcBucket) ;
+		
+		try {
+			// upload a file to the source bucket
+			String dir = System.getProperty("user.dir");
+			String absFilePath = dir + "/testdata/" + key;
+
+			PutAuthRet putAuthRet = rs.putAuth();
+			String authorizedUrl = putAuthRet.getUrl();
+
+			@SuppressWarnings("deprecation")
+			PutFileRet putFileRet = RSClient.putFile(authorizedUrl, srcBucket,
+					srcKey, "", absFilePath, "", "");
+			assertTrue(putFileRet.ok()&& expectedHash.equals(putFileRet.getHash()));
+
+			// copy
+			rs = new RSService(conn, srcBucket);
+			CallRet moveRet = rs.move(entryUriSrc, entryUriDest);
+			assertTrue(moveRet.ok());
+
+			// stat to check the result
+			// stat the src, should not exist!
+			rs = new RSService(conn, srcBucket);
+			StatRet srcStatRet = rs.stat(srcKey);
+			assertTrue(!srcStatRet.ok());
+
+			// stat the dest, should exist!
+			rs = new RSService(conn, destBucket);
+			StatRet statRet = rs.stat(destKey);
+			assertTrue(statRet.ok() && expectedHash.equals(statRet.getHash()));
+
+		} finally {
+			// delete file in the dest bucket
+			rs = new RSService(conn, destBucket);
+			DeleteRet destDeleteRet = rs.delete(destKey);
+			assertTrue(destDeleteRet.ok());
+		}
+	}
+	
+	public void testCopy() throws Exception {
+		
+		String srcKey = "src-key-copy";
+		String destKey = "dest-key-copy";
+		String entryUriSrc = srcBucket + ":" + srcKey;
+		String entryUriDest = destBucket + ":" + destKey;
+		DigestAuthClient conn = new DigestAuthClient();
+		rs = new RSService(conn, srcBucket) ;
+		
+		try {
+			// upload a file to the source bucket
+			String dir = System.getProperty("user.dir");
+			String absFilePath = dir + "/testdata/" + key;
+
+			PutAuthRet putAuthRet = rs.putAuth();
+			String authorizedUrl = putAuthRet.getUrl();
+
+			@SuppressWarnings("deprecation")
+			PutFileRet putFileRet = RSClient.putFile(authorizedUrl, srcBucket,
+					srcKey, "", absFilePath, "", "");
+			assertTrue(putFileRet.ok()&& expectedHash.equals(putFileRet.getHash()));
+
+			// copy
+			rs = new RSService(conn, srcBucket);
+			CallRet copyRet = rs.copy(entryUriSrc, entryUriDest);
+			assertTrue(copyRet.ok());
+
+			// stat to check the result
+			// file should exist in the source bucket
+			rs = new RSService(conn, srcBucket);
+			StatRet srcStatRet = rs.stat(srcKey);
+			assertTrue(srcStatRet.ok()&& expectedHash.equals(srcStatRet.getHash()));
+
+			// file should exist in the dest bucket
+			rs = new RSService(conn, destBucket);
+			StatRet statRet = rs.stat(destKey);
+			assertTrue(statRet.ok() && expectedHash.equals(statRet.getHash())) ;
+			
+		} finally {
+			// delete the file in the source/dest bucket
+			rs = new RSService(conn, srcBucket);
+			DeleteRet srcDeleteRet = rs.delete(srcKey);
+			assertTrue(srcDeleteRet.ok());
+
+			rs = new RSService(conn, destBucket);
+			DeleteRet destDeleteRet = rs.delete(destKey);
+			assertTrue(destDeleteRet.ok());
+		}
+	}
+
+	public void testBuckets() throws Exception {
+		// just test ret.ok().
+		// it is not a good way to compare wheter the result contains all the 
+		// existing buckets. Because it's a shared account, other users may also
+		// create or drop bucket at the same time.
+		BucketsRet ret = rs.buckets();
+		assertTrue(ret.ok());
 	}
 	
 }
