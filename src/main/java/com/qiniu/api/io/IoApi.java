797ebe5afca956856040cc590de5701ee946f110
Simon <simon.liubin@gmail.com>
2013-11-21 17:32:11 +0800

    1.忽略key时，使用文件的etag作为key 2.将命名统一为put，兼容以前到调用方式

--- src/main/java/com/qiniu/api/io/IoApi.java
+++ src/main/java/com/qiniu/api/io/IoApi.java
@@ -7,9 +7,11 @@ import java.util.zip.CRC32;
 import java.util.zip.CheckedInputStream;
 
 import org.apache.http.entity.mime.MultipartEntity;
+import org.apache.http.entity.mime.content.AbstractContentBody;
 import org.apache.http.entity.mime.content.FileBody;
 import org.apache.http.entity.mime.content.InputStreamBody;
 import org.apache.http.entity.mime.content.StringBody;
+
 import com.qiniu.api.config.Config;
 import com.qiniu.api.net.CallRet;
 import com.qiniu.api.net.Client;
@@ -17,80 +19,22 @@ import com.qiniu.api.net.Client;
 import java.nio.charset.Charset;
 
 public class IoApi {
-	
-	public static final String UNDEFINED_KEY = "?";
 	public static final int NO_CRC32 = 0;
 	public static final int AUTO_CRC32 = 1;
 	public static final int WITH_CRC32 = 2;
-	
-	private static PutRet put(String uptoken, String key, File file,
+
+	public static PutRet put(String uptoken, String key, String fileName,
 			PutExtra extra) {
-		
+		File file = new File(fileName);
+		return put(uptoken, key, file, extra);
+	}
+
+	public static PutRet put(String uptoken, String key, File file,
+			PutExtra extra) {
 		if (!file.exists() || !file.canRead()) {
 			return new PutRet(new CallRet(400, new Exception(
 					"File does not exist or not readable.")));
 		}
-		if (key == null) {
-			key = UNDEFINED_KEY;
-		}
-		MultipartEntity requestEntity = new MultipartEntity();
-		try {
-			requestEntity.addPart("token", new StringBody(uptoken));
-			FileBody fileBody = new FileBody(file);
-			requestEntity.addPart("file", fileBody);
-			requestEntity.addPart("key", new StringBody(key,Charset.forName("utf-8")));
-			if (extra.checkCrc != NO_CRC32) {
-				if (extra.crc32 == 0) {
-					return new PutRet(new CallRet(400, new Exception("no crc32 specified!")));
-				}
-				requestEntity.addPart("crc32", new StringBody(extra.crc32 + ""));
-			}	
-		} catch (Exception e) {
-			e.printStackTrace();
-			return new PutRet(new CallRet(400, e));
-		}
-
-		String url = Config.UP_HOST;
-		CallRet ret = new Client().callWithMultiPart(url, requestEntity);
-		return new PutRet(ret);
-	}
-	
-	private static PutRet putStream(String uptoken, String key, InputStream reader,PutExtra extra) {
-		MultipartEntity requestEntity = new MultipartEntity();
-		try {
-			requestEntity.addPart("token", new StringBody(uptoken));
-			InputStreamBody inputBody= new InputStreamBody(reader,key);
-			requestEntity.addPart("file", inputBody);
-			requestEntity.addPart("key", new StringBody(key,Charset.forName("utf-8")));
-			if (extra.checkCrc != NO_CRC32) {
-				if (extra.crc32 == 0) {
-					return new PutRet(new CallRet(400, new Exception("no crc32 specified!")));
-				}
-				requestEntity.addPart("crc32", new StringBody(extra.crc32 + ""));
-			}
-		} catch (Exception e) {
-			e.printStackTrace();
-			return new PutRet(new CallRet(400, e));
-		}
-
-		String url = Config.UP_HOST;
-		CallRet ret = new Client().callWithMultiPart(url, requestEntity);
-		return new PutRet(ret);
-	}
-	
-	
-	public static PutRet Put(String uptoken,String key,InputStream reader,PutExtra extra)
-	{		
-		if (key == null) {
-			key = UNDEFINED_KEY;
-		}
-		PutRet ret = putStream(uptoken,key,reader,extra);
-		return ret;
-	}
-	
-	
-	public static PutRet putFile(String uptoken, String key, String fileName, PutExtra extra) {
-		File file=new File(fileName);
 		if (extra.checkCrc == AUTO_CRC32) {
 			try {
 				extra.crc32 = getCRC32(file);
@@ -98,9 +42,61 @@ public class IoApi {
 				return new PutRet(new CallRet(400, e));
 			}
 		}
-		return put(uptoken, key, file, extra);
+		FileBody fileBody = new FileBody(file);
+		return put(uptoken, key, fileBody, extra);
 	}
-	
+
+	private static PutRet put(String uptoken, String key,
+			AbstractContentBody fileBody, PutExtra extra) {
+		MultipartEntity requestEntity = new MultipartEntity();
+		try {
+			if (extra.checkCrc != NO_CRC32) {
+				if (extra.crc32 == 0) {
+					return new PutRet(new CallRet(400, new Exception(
+							"no crc32 specified!")));
+				}
+				requestEntity
+						.addPart("crc32", new StringBody(extra.crc32 + ""));
+			}
+			requestEntity.addPart("token", new StringBody(uptoken));
+			requestEntity.addPart("file", fileBody);
+			if (!isNullOrEmpty(key)) {
+				requestEntity.addPart("key",
+						new StringBody(key, Charset.forName("utf-8")));
+			}
+		} catch (Exception e) {
+			return new PutRet(new CallRet(400, e));
+		}
+
+		String url = Config.UP_HOST;
+		CallRet ret = new Client().callWithMultiPart(url, requestEntity);
+		return new PutRet(ret);
+	}
+
+	private static boolean isNullOrEmpty(String str) {
+		return null == str || str.trim().isEmpty();
+	}
+
+	public static PutRet put(String uptoken, String key, InputStream reader,
+			PutExtra extra) {
+		// inputBody's fileName can not be null or empty
+		String fileName = isNullOrEmpty(key) ? "?" : key;
+		InputStreamBody inputBody = new InputStreamBody(reader, fileName);
+		return put(uptoken, key, inputBody, extra);
+	}
+
+	@Deprecated
+	public static PutRet Put(String uptoken, String key, InputStream reader,
+			PutExtra extra) {
+		return put(uptoken, key, reader, extra);
+	}
+
+	@Deprecated
+	public static PutRet putFile(String uptoken, String key, String fileName,
+			PutExtra extra) {
+		return put(uptoken, key, fileName, extra);
+	}
+
 	private static long getCRC32(File file) throws Exception {
 		CRC32 crc32 = new CRC32();
 		FileInputStream in = null;
@@ -109,19 +105,21 @@ public class IoApi {
 		try {
 			in = new FileInputStream(file);
 			checkedInputStream = new CheckedInputStream(in, crc32);
-			while (checkedInputStream.read() != -1) {
+			byte[] buffer = new byte[1024];
+			while (checkedInputStream.read(buffer) != -1) {
 			}
 			crc = crc32.getValue();
 		} finally {
-			if (in != null) {
-				in.close();
-				in = null;
-			}
 			if (checkedInputStream != null) {
 				checkedInputStream.close();
 				checkedInputStream = null;
 			}
+			if (in != null) {
+				in.close();
+				in = null;
+			}
 		}
 		return crc;
 	}
+
 }
