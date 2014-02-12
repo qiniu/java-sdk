05deb3ec7cceae274c0d042cc57a7f378de1e1da
simon-liubin <simon.liubin@gmail.com>
2014-02-12 15:08:34 +0800

    修改格式

--- src/test/java/com/qiniu/testing/HttpClientTimeOutTest.java
+++ src/test/java/com/qiniu/testing/HttpClientTimeOutTest.java
@@ -12,17 +12,17 @@ import org.apache.http.conn.ConnectTimeoutException;
 import com.qiniu.api.config.Config;
 import com.qiniu.api.net.Http;
 
-public class HttpClientTimeOutTest extends TestCase{
+public class HttpClientTimeOutTest extends TestCase {
 	private int CONNECTION_TIMEOUT;
 	private int SO_TIMEOUT;
-	
+
 	@Override
 	public void setUp() {
 		CONNECTION_TIMEOUT = Config.CONNECTION_TIMEOUT;
 		SO_TIMEOUT = Config.SO_TIMEOUT;
 		Http.setClient(null);
 	}
-	
+
 	@Override
 	public void tearDown() {
 		Config.CONNECTION_TIMEOUT = CONNECTION_TIMEOUT;
@@ -33,47 +33,48 @@ public class HttpClientTimeOutTest extends TestCase{
 	public void testCONNECTION_TIMEOUT() {
 		Throwable tx = null;
 		long s = 0;
-		try{
+		try {
 			Config.CONNECTION_TIMEOUT = 5;
 			Config.SO_TIMEOUT = 20 * 1000;
-			
+
 			HttpClient client = Http.getClient();
-			HttpGet httpget = new HttpGet("http://kyfxbl.iteye.com/blog/1616849"); 
-			
+			HttpGet httpget = new HttpGet(
+					"http://kyfxbl.iteye.com/blog/1616849");
+
 			s = System.currentTimeMillis();
 			HttpResponse ret = client.execute(httpget);
-			
-			 fail("应该按预期抛出异常 ConnectTimeoutException，测试失败");
-		}catch(Exception e){
+
+			fail("应该按预期抛出异常 ConnectTimeoutException，测试失败");
+		} catch (Exception e) {
 			long end = System.currentTimeMillis();
 			System.out.println("CONNECTION_TIMEOUT test : " + (end - s));
 			tx = e;
 		}
-		
+
 		assertNotNull(tx.getMessage());
-        assertEquals(ConnectTimeoutException.class, tx.getClass());
+		assertEquals(ConnectTimeoutException.class, tx.getClass());
 	}
 
 	public void testSO_TIMEOUT() {
 		Throwable tx = null;
 		long s = 0;
-		try{
+		try {
 			Config.CONNECTION_TIMEOUT = 20 * 1000;
-			Config.SO_TIMEOUT = 5;
-			
+			Config.SO_TIMEOUT = 5000;
+
 			HttpClient client = Http.getClient();
 			HttpGet httpget = new HttpGet("http://www.baidu.com");
-			
+
 			s = System.currentTimeMillis();
 			HttpResponse ret = client.execute(httpget);
 			fail("应该按预期抛出异常 SocketTimeoutException，测试失败");
-		}catch(Exception e){
+		} catch (Exception e) {
 			long end = System.currentTimeMillis();
 			System.out.println("SO_TIMEOUT test : " + (end - s));
 			tx = e;
 		}
 		assertNotNull(tx.getMessage());
-        assertEquals(SocketTimeoutException.class, tx.getClass());
+		assertEquals(SocketTimeoutException.class, tx.getClass());
 	}
 
 }
