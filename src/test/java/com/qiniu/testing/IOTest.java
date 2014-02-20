084bac4e63634ed281d887385a4fea8699d890c4
simon-liubin <simon.liubin@gmail.com>
2014-02-20 10:57:43 +0800

    去掉引入的junit4

--- src/test/java/com/qiniu/testing/IOTest.java
+++ src/test/java/com/qiniu/testing/IOTest.java
@@ -1,9 +1,5 @@
 package com.qiniu.testing;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertFalse;
-import static org.junit.Assert.assertTrue;
-
 import java.io.*;
 import java.util.HashMap;
 import java.util.Map;
