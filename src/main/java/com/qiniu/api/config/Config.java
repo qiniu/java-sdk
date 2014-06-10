ff3e0d53bcfb224a3b72e5d2c04b23686382c8fe
Bai Long <slongbai@gmail.com>
2014-06-10 23:05:23 +0800

    Update Config.java

--- src/main/java/com/qiniu/api/config/Config.java
+++ src/main/java/com/qiniu/api/config/Config.java
@@ -9,7 +9,7 @@ public class Config {
 	/**本地检测不通过、程序抛异常，设置 CallRet 的 statusCode 为此错误码*/
 	public static final int ERROR_CODE = 0;
 
-	public static final String VERSION = "6.0.5";
+	public static final String VERSION = "6.1.5";
 
 	/**
 	 * You can get your accesskey from <a href="https://dev.qiniutek.com"
