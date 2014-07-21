b3bb32f31bb55b62795d87bca3d6a2af117db66a
longbai <slongbai@gmail.com>
2014-07-21 18:13:42 +0800

    remove useless doc

--- src/main/java/com/qiniu/api/config/Config.java
+++ src/main/java/com/qiniu/api/config/Config.java
@@ -11,16 +11,8 @@ public class Config {
 
 	public static final String VERSION = "6.1.5";
 
-	/**
-	 * You can get your accesskey from <a href="https://dev.qiniutek.com"
-	 * target="blank"> https://dev.qiniutek.com </a>
-	 */
 	public static String ACCESS_KEY = "<Please apply your access key>";
 
-	/**
-	 * You can get your accesskey from <a href="https://dev.qiniutek.com"
-	 * target="blank"> https://dev.qiniutek.com </a>
-	 */
 	public static String SECRET_KEY = "<Apply your secret key here, and keep it secret!>";
 
 	public static String RS_HOST = "http://rs.qbox.me";
