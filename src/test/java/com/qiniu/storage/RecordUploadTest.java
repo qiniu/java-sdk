219012e876da966fe16324c73084d2a8932f32b0
simon-liubin <simon.liubin@gmail.com>
2015-03-30 21:00:54 +0800

    保存分块记录

---
+++ src/test/java/com/qiniu/storage/RecordUploadTest.java
@@ -0,0 +1,199 @@
+package com.qiniu.storage;
+
+import com.qiniu.TempFile;
+import com.qiniu.TestConfig;
+import com.qiniu.common.Config;
+import com.qiniu.common.QiniuException;
+import com.qiniu.http.Client;
+import com.qiniu.http.Response;
+import com.qiniu.storage.model.DefaultPutRet;
+import com.qiniu.storage.persistent.FileRecorder;
+import com.qiniu.util.StringUtils;
+import com.qiniu.util.UrlSafeBase64;
+import org.junit.Test;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.Random;
+import java.util.concurrent.*;
+
+import static org.junit.Assert.*;
+import static org.junit.Assert.assertTrue;
+
+/**
+ * Created by Simon on 2015/3/30.
+ */
+public class RecordUploadTest {
+
+    private boolean isDone = false;
+    private Response response = null;
+
+    private void template(int size) throws IOException {
+        isDone = false;
+        ExecutorService threadPool = Executors.newSingleThreadExecutor();
+        final String expectKey = "\r\n?&r=" + size + "k";
+        final File f = TempFile.createFile(size);
+        final String token = TestConfig.testAuth.uploadToken(TestConfig.bucket, expectKey);
+        final FileRecorder recorder = new FileRecorder(f.getParentFile());
+        final RecordKeyGenerator keyGen = new RecordKeyGenerator(){
+            @Override
+            public String gen(String key, File file) {
+                return key + "_._" + file.getAbsolutePath();
+            }
+        };
+        final String recordKey = keyGen.gen(expectKey, f);
+
+        UploadManager uploadManager = new UploadManager(recorder, keyGen);
+//        Response res = uploadManager.put(f, expectKey, token);
+
+        Up up = new Up(uploadManager, f, expectKey, token);
+
+
+        // 显示断点记录文件
+        Thread showRecord = new Thread(){
+            public void run(){
+                for(;;) {
+                    doSleep(30);
+                    showRecord("normal: ", recorder, recordKey);
+                }
+            }
+        };
+        showRecord.setDaemon(true);
+        showRecord.start();
+
+        final String p = f.getParentFile().getAbsolutePath() + "\\" + UrlSafeBase64.encodeToString(recordKey);
+        System.out.println(p);
+        System.out.println(new File(p).exists());
+
+        final Random r = new Random();
+
+        for(int i = 10; i > 0; i--) {
+            final int t = r.nextInt(100) + 80;
+            System.out.println(i + "  :  " + t);
+            final Future<Response> future = threadPool.submit(up);
+            // 中断线程
+             new Thread(){
+                public void run(){
+                    if(!new File(p).exists()) {
+                        doSleep(t);// 百毫秒
+                        for (; ; ) {
+                            System.out.println(Thread.currentThread().getId() + " :  future.isDone() : " + future.isDone());
+                            if (!future.isDone()) {
+                                future.cancel(true);
+                            }
+                            System.out.println(Thread.currentThread().getId() + " : future.isCancelled : " + future.isCancelled());
+                            if (future.isCancelled() || future.isDone()) {
+                                break;
+                            }
+                            doSleep(5);
+                        }
+                    }
+                }
+            }.start();
+
+            showRecord("new future: ", recorder, recordKey);
+
+            try {
+                Response res = future.get();
+                response = res;
+                showRecord("done: ", recorder, recordKey);
+                System.out.println("break");
+                break;
+            } catch (Exception e) {
+                System.out.println("Exception");
+                System.out.println(Thread.currentThread().getId() + " : future.isCancelled : " + future.isCancelled());
+//                e.printStackTrace();
+                if(isDone){
+                    break;
+                }
+            }
+            doSleep(30);
+            System.out.println(p);
+            System.out.println(new File(p).exists());
+        }
+
+        TempFile.remove(f);
+        assertFalse(new File(p).exists());
+        assertNotNull(response);
+        assertTrue(response.isOK());
+
+        showRecord("nodata: ", recorder, recordKey);
+    }
+
+    private void showRecord(String pre, FileRecorder recorder, String recordKey) {
+        try {
+            String jsonStr = pre;
+            byte[] data = recorder.get(recordKey);
+            if (data != null) {
+                jsonStr += new String(data);
+            }
+            System.out.println(jsonStr);
+        } catch (Exception e) {
+            //e.printStackTrace();
+        }
+    }
+
+    private void doSleep(int bm){
+        try {
+            Thread.sleep(100 * bm);
+        } catch (InterruptedException e) {
+            //e.printStackTrace();
+        }
+    }
+
+    class Up implements Callable<Response> {
+        private final UploadManager uploadManager;
+        private final File file;
+        private final String key;
+        private final String token;
+        public Up(UploadManager uploadManager, File file, String key, String token){
+            this.uploadManager = uploadManager;
+            this.file = file;
+            this.key = key;
+            this.token = token;
+        }
+        @Override
+        public Response call() throws Exception {
+            Response res = uploadManager.put(file, key, token);
+            System.out.println("up:  " + res);
+            System.out.println("up:  " + res.bodyString());
+            isDone = true;
+            response = res;
+            return res;
+        }
+    }
+
+    @Test
+    public void test1K() throws Throwable {
+        template(1);
+    }
+
+    @Test
+    public void test600k() throws Throwable {
+        template(600);
+    }
+
+    @Test
+    public void test4M() throws Throwable {
+        if (TestConfig.isTravis()) {
+            return;
+        }
+        template(1024 * 4);
+    }
+
+    @Test
+    public void test8M1k() throws Throwable {
+        if (TestConfig.isTravis()) {
+            return;
+        }
+        template(1024 * 8 + 1);
+    }
+
+    @Test
+    public void test25M1k() throws Throwable {
+        if (TestConfig.isTravis()) {
+            return;
+        }
+        template(1024 * 25 + 1);
+    }
+}
