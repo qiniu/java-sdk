1b8246749ba0027bed028aea126f31117553d4ac
clouddxy <clouddxy@126.com>
2016-04-29 13:34:18 +0800

    add some demos

---
+++ examples/upload.java
@@ -0,0 +1,51 @@
+import com.qiniu.util.Auth;
+import java.io.IOException;
+import com.qiniu.common.QiniuException;
+import com.qiniu.http.Response;
+import com.qiniu.storage.UploadManager;
+
+public class UploadDemo {
+  //设置好账号的ACCESS_KEY和SECRET_KEY
+  String ACCESS_KEY = "Access_Key";
+  String SECRET_KEY = "Secret_Key";
+  //要上传的空间
+  String bucketname = "Bucket_Name";
+  //上传到七牛后保存的文件名
+  String key = "my-java.png";
+  //上传文件的路径
+  String FilePath = "/.../...";
+
+  //密钥配置
+  Auth auth = Auth.create(ACCESS_KEY, SECRET_KEY);
+  //创建上传对象
+  UploadManager uploadManager = new UploadManager();
+
+  //简单上传，使用默认策略，只需要设置上传的空间名就可以了
+  public String getUpToken(){
+      return auth.uploadToken(bucketname);
+  }
+
+  public void upload() throws IOException{
+    try {
+      //调用put方法上传
+      Response res = uploadManager.put(FilePath, key, getUpToken());
+      //打印返回的信息
+      System.out.println(res.bodyString()); 
+      } catch (QiniuException e) {
+          Response r = e.response;
+          // 请求失败时打印的异常的信息
+          System.out.println(r.toString());
+          try {
+              //响应的文本信息
+            System.out.println(r.bodyString());
+          } catch (QiniuException e1) {
+              //ignore
+          }
+      }       
+  }
+
+  public static void main(String args[]) throws IOException{  
+    new UploadDemo().upload();
+  }
+
+}
