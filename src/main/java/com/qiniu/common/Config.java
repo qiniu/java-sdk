2c04696c738d126bc37dd226420195023afbde03
longbai <slongbai@gmail.com>
2016-09-02 11:25:17 +0800

    update verison [ci skip]

--- src/main/java/com/qiniu/common/Config.java
+++ src/main/java/com/qiniu/common/Config.java
@@ -9,7 +9,7 @@ import java.nio.charset.Charset;
 
 public final class Config {
 
-    public static final String VERSION = "7.1.2";
+    public static final String VERSION = "7.1.3";
     /**
      * 断点上传时的分块大小(默认的分块大小, 不允许改变)
      */
