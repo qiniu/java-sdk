c37ec1a5a43b29baa0c04868c262720df1ed4976
longbai <slongbai@gmail.com>
2016-09-21 19:48:25 +0800

    recorder

--- src/main/java/com/qiniu/storage/Recorder.java
+++ src/main/java/com/qiniu/storage/Recorder.java
@@ -30,5 +30,12 @@ public interface Recorder {
      */
     void del(String key);
 
+    /**
+     * 根据服务器的key和本地文件名生成持久化纪录的key
+     *
+     * @param key  服务器的key
+     * @param file 本地文件名
+     * @return 持久化上传纪录的key
+     */
     String recorderKeyGenerate(String key, File file);
 }
