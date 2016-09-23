cabe3469c616c48bb1971107a4a5b653c788ad9d
longbai <slongbai@gmail.com>
2016-09-23 21:39:25 +0800

    pili integrate done

---
+++ src/main/java/com/qiniu/streaming/model/StreamAttribute.java
@@ -0,0 +1,11 @@
+package com.qiniu.streaming.model;
+
+/**
+ * Created by bailong on 16/9/22.
+ */
+public final class StreamAttribute {
+    public long createdAt;
+    public long updatedAt;
+    public long expireAt;
+    public long disabledTill;
+}
