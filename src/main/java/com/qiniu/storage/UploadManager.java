eb5c15eb4bac09d15a185fa614b88bf74ee1eea6
longbai <slongbai@gmail.com>
2016-11-04 21:15:39 +0800

    add stream upload

--- src/main/java/com/qiniu/storage/UploadManager.java
+++ src/main/java/com/qiniu/storage/UploadManager.java
@@ -7,6 +7,7 @@ import com.qiniu.util.StringMap;
 
 import java.io.File;
 import java.io.IOException;
+import java.io.InputStream;
 
 /**
  * 七牛文件上传管理器
@@ -173,7 +174,17 @@ public final class UploadManager {
         return uploader.upload();
     }
 
-
+    /**
+     * 异步上传数据
+     *
+     * @param data     上传的数据
+     * @param key      上传数据保存的文件名
+     * @param token    上传凭证
+     * @param params   自定义参数，如 params.put("x:foo", "foo")
+     * @param mime     指定文件mimetype
+     * @param checkCrc 是否验证crc32
+     * @param handler  上传完成的回调函数
+     */
     public void asyncPut(final byte[] data, final String key, final String token, StringMap params,
                          String mime, boolean checkCrc, UpCompletionHandler handler) throws IOException {
         checkArgs(key, data, null, token);
@@ -182,5 +193,30 @@ public final class UploadManager {
         }
         params = filterParam(params);
         new FormUploader(client, token, key, data, params, mime, checkCrc, configuration).asyncUpload(handler);
+    }
+
+    /**
+     * 流式上传，通常情况建议文件上传，可以使用持久化的断点记录。
+     *
+     * @param stream sha
+     * @param key    上传文件保存的文件名
+     * @param token  上传凭证
+     * @param params 自定义参数，如 params.put("x:foo", "foo")
+     * @param mime   指定文件mimetype
+     */
+    public Response put(InputStream stream, String key, String token, StringMap params,
+                        String mime) throws QiniuException {
+        String message = null;
+        if (stream == null) {
+            message = "no input data";
+        } else if (token == null || token.equals("")) {
+            message = "no token";
+        }
+        if (message != null) {
+            throw new IllegalArgumentException(message);
+        }
+        StreamUploader uploader = new StreamUploader(client, token, key, stream,
+                params, mime, configuration);
+        return uploader.upload();
     }
 }
