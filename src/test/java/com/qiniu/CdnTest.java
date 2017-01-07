331dc5a37fd1e037b4c6f0155b627a687a522abf
Jemy <jemygraw@gmail.com>
2017-01-07 14:08:42 +0800

    add get cdn log list and create anti leech url based on timestamp

--- src/test/java/com/qiniu/CdnTest.java
+++ src/test/java/com/qiniu/CdnTest.java
@@ -69,4 +69,56 @@ public class CdnTest {
             Assert.fail();
         }
     }
+
+    @Test
+    public void testGetCdnLogList() {
+        CdnManager c = new CdnManager(TestConfig.testAuth);
+        Response r = null;
+        String[] domains = {TestConfig.domain};
+        String logDate = "2017-01-01";
+
+        try {
+            r = c.getCdnLogList(domains, logDate);
+            Assert.assertEquals(200, r.statusCode);
+        } catch (QiniuException e) {
+            e.printStackTrace();
+            Assert.fail();
+        }
+    }
+
+    @Test
+    public void testCreateAntileechUrlBasedOnTimestampSimple() {
+        String host = "http://img.abc.com";
+        String fileName = "2017/01/07/测试.png";
+        String queryString = "";
+        long deadline = System.currentTimeMillis() / 1000 + 3600;
+        String encryptKey = "";
+        String signedUrl;
+        try {
+            signedUrl = CdnManager.createStandardAntileechUrlBasedOnTimestamp(host, fileName,
+                    queryString, encryptKey, deadline);
+            System.out.println(signedUrl);
+        } catch (Exception ex) {
+            ex.printStackTrace();
+            Assert.fail();
+        }
+    }
+
+    @Test
+    public void testCreateAntileechUrlBasedOnTimestampWithQueryString() {
+        String host = "http://video.abc.com";
+        String fileName = "测试.mp4";
+        String queryString = "name=七牛&year=2017";
+        long deadline = System.currentTimeMillis() / 1000 + 3600;
+        String encryptKey = "";
+        String signedUrl;
+        try {
+            signedUrl = CdnManager.createStandardAntileechUrlBasedOnTimestamp(host, fileName,
+                    queryString, encryptKey, deadline);
+            System.out.println(signedUrl);
+        } catch (Exception ex) {
+            ex.printStackTrace();
+            Assert.fail();
+        }
+    }
 }
