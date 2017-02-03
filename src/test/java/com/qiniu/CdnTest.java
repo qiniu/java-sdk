f29923e87f9c1f73af3a80cc7c08fdc0f7a8c70a
Jemy <jemygraw@gmail.com>
2017-02-03 22:29:06 +0800

    parse the cdn related api response to class objects

--- src/test/java/com/qiniu/CdnTest.java
+++ src/test/java/com/qiniu/CdnTest.java
@@ -1,8 +1,8 @@
 package com.qiniu;
 
 import com.qiniu.cdn.CdnManager;
+import com.qiniu.cdn.CdnResult;
 import com.qiniu.common.QiniuException;
-import com.qiniu.http.Response;
 import com.qiniu.util.StringMap;
 import org.junit.Assert;
 import org.junit.Test;
@@ -14,10 +14,10 @@ public class CdnTest {
     @Test
     public void testRefresh() {
         CdnManager c = new CdnManager(TestConfig.testAuth);
-        Response r = null;
+        CdnResult.RefreshResult r = null;
         try {
             r = c.refreshUrls(new String[]{"http://javasdk.qiniudn.com/gopher.jpg"});
-            Assert.assertEquals(200, r.statusCode);
+            Assert.assertEquals(200, r.getCode());
         } catch (QiniuException e) {
             e.printStackTrace();
             Assert.fail();
@@ -27,10 +27,10 @@ public class CdnTest {
     @Test
     public void testPrefetch() {
         CdnManager c = new CdnManager(TestConfig.testAuth);
-        Response r = null;
+        CdnResult.PrefetchResult r = null;
         try {
             r = c.prefetchUrls(new String[]{"http://javasdk.qiniudn.com/gopher.jpg"});
-            Assert.assertEquals(200, r.statusCode);
+            Assert.assertEquals(200, r.getCode());
         } catch (QiniuException e) {
             e.printStackTrace();
             Assert.fail();
@@ -40,14 +40,14 @@ public class CdnTest {
     @Test
     public void testGetBandwidth() {
         CdnManager c = new CdnManager(TestConfig.testAuth);
-        Response r = null;
+        CdnResult.BandwidthResult r = null;
         String[] domains = {TestConfig.domain};
         String startDate = "2017-01-01";
         String endDate = "2017-01-06";
         String granularity = "day";
         try {
             r = c.getBandwidthData(domains, startDate, endDate, granularity);
-            Assert.assertEquals(200, r.statusCode);
+            Assert.assertEquals(200, r.getCode());
         } catch (QiniuException e) {
             e.printStackTrace();
             Assert.fail();
@@ -57,14 +57,14 @@ public class CdnTest {
     @Test
     public void testGetFlux() {
         CdnManager c = new CdnManager(TestConfig.testAuth);
-        Response r = null;
+        CdnResult.FluxResult r = null;
         String[] domains = {TestConfig.domain};
         String startDate = "2017-01-01";
         String endDate = "2017-01-06";
         String granularity = "day";
         try {
             r = c.getFluxData(domains, startDate, endDate, granularity);
-            Assert.assertEquals(200, r.statusCode);
+            Assert.assertEquals(200, r.getCode());
         } catch (QiniuException e) {
             e.printStackTrace();
             Assert.fail();
@@ -74,13 +74,13 @@ public class CdnTest {
     @Test
     public void testGetCdnLogList() {
         CdnManager c = new CdnManager(TestConfig.testAuth);
-        Response r = null;
+        CdnResult.LogListResult r = null;
         String[] domains = {TestConfig.domain};
         String logDate = "2017-01-01";
 
         try {
             r = c.getCdnLogList(domains, logDate);
-            Assert.assertEquals(200, r.statusCode);
+            Assert.assertEquals(true, r.getData().size() > 0);
         } catch (QiniuException e) {
             e.printStackTrace();
             Assert.fail();
