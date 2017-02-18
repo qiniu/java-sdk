7da08d506ae132c93a8854dd3c040c616e76c429
Jemy <jemygraw@gmail.com>
2017-02-18 15:23:47 +0800

    [ISSUE-276] add more fields for BatchStatus

--- src/main/java/com/qiniu/storage/model/BatchStatus.java
+++ src/main/java/com/qiniu/storage/model/BatchStatus.java
@@ -2,7 +2,15 @@ package com.qiniu.storage.model;
 
 /**
  * 定义批量请求的状态码
+ * 参考文档：<a href="https://developer.qiniu.com/kodo/api/batch">批量操作</a>
  */
 public final class BatchStatus {
+    /**
+     * 批量请求的每个命令的执行结果状态码
+     */
     public int code;
+    /**
+     * 批量请求的每个命令返回的结果
+     */
+    public BatchOpData data;
 }
