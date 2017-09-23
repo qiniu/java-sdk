c368b918d1afb8f101eb347c357d2ebd895de48b
jemygraw <jemygraw@gmail.com>
2017-09-23 12:17:20 +0800

    remove the useless prefix host detection

--- src/main/java/com/qiniu/common/Constants.java
+++ src/main/java/com/qiniu/common/Constants.java
@@ -9,7 +9,7 @@ public final class Constants {
     /**
      * 版本号
      */
-    public static final String VERSION = "7.2.7";
+    public static final String VERSION = "7.2.8";
     /**
      * 块大小，不能改变
      */
