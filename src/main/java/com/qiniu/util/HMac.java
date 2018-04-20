e16bd80cf68d5039b8ce920c09f8013133d6d791
JemyCheung <zhangwei@qiniu.com>
2018-04-20 14:29:04 +0800

    format

--- src/main/java/com/qiniu/util/HMac.java
+++ src/main/java/com/qiniu/util/HMac.java
@@ -5,10 +5,12 @@ import javax.crypto.SecretKey;
 import javax.crypto.spec.SecretKeySpec;
 
 
-public class HMac {
+public final class HMac {
     private static final String MAC_NAME = "HmacSHA1";
     private static final String UTF8 = "UTF-8";
+    private HMac(){
 
+    }
     public static byte[] hmacSHA1Encrypt(String dataStr, String secretKeyStr) throws Exception {
         SecretKey secretKeySpec = new SecretKeySpec(secretKeyStr.getBytes(), MAC_NAME);
         //生成一个指定 Mac 算法 的 Mac 对象
