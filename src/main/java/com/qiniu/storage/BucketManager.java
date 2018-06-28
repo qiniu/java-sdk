2c3450841740ea22ded0627eb1f0417a5c9de728
Sxci <sxci@foxmail.com>
2018-06-28 16:05:48 +0800

    stat 接口没有响应 key 这个属性  [ci skip]

--- src/main/java/com/qiniu/storage/BucketManager.java
+++ src/main/java/com/qiniu/storage/BucketManager.java
@@ -169,9 +169,9 @@ public final class BucketManager {
      */
     public FileInfo stat(String bucket, String fileKey) throws QiniuException {
         Response r = rsGet(bucket, String.format("/stat/%s", encodedEntry(bucket, fileKey)));
-        FileInfo info = r.jsonToObject(FileInfo.class);
-        info.key = fileKey;
-        return info;
+        FileInfo fileInfo = r.jsonToObject(FileInfo.class);
+        fileInfo.key = fileKey;
+        return fileInfo;
     }
 
     /**
