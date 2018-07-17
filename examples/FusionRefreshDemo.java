cce51caab99c30b0eee600ba6ea4f3209c0e7565
softbs@outlook.com <softbs@outlook.com>
2018-07-17 15:43:35 +0800

    cdn接口刷新

--- examples/FusionRefreshDemo.java
+++ examples/FusionRefreshDemo.java
@@ -1,4 +1,4 @@
-package com.example.fusion;
+package com.examples;
 
 import com.google.gson.Gson;
 import com.qiniu.http.Client;
@@ -13,27 +13,24 @@ public class FusionRefreshDemo {
 
     protected static final org.slf4j.Logger logger = LoggerFactory.getLogger(FusionRefreshDemo.class);
 
-    public static  void main(String[] args) throws Exception {
-
+    public static void main(String[] args) throws Exception {
         String ACCESS_KEY = "Access_Key";
         String SECRET_KEY = "Secret_Key";
         //要上传的空间
         String bucketname = "Bucket_Name";
-        Auth auth =  Auth.create(ACCESS_KEY,SECRET_KEY);
+        Auth auth = Auth.create(ACCESS_KEY, SECRET_KEY);
         // 构造post请求body
         Gson gson = new Gson();
-        Map<String,String[]> m = new HashMap();
+        Map<String, String[]> m = new HashMap();
         String[] urls = {"http://neko.chentao.qiniuts.com/1.MP4",
-                         "http://neko.chentao.qiniuts.com/1.mp4"};
-        m.put("urls",urls);
+                "http://neko.chentao.qiniuts.com/1.mp4"};
+        m.put("urls", urls);
         String paraR = gson.toJson(m);
         byte[] bodyByte = paraR.getBytes();
-
         // 获取签名
         String url = "http://fusion.qiniuapi.com/v2/tune/refresh";
         String accessToken = (String) auth.authorizationV2(url, "POST", bodyByte, "application/json")
                 .get("Authorization");
-
         Client client = new Client();
         StringMap headers = new StringMap();
         headers.put("Authorization", accessToken);
