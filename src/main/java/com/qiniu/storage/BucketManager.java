1956374d5af9f1dea9ede8b8bd9b67a67a0e8ca1
wubingheng <wubinghengajw@outlook.com>
2019-01-24 21:43:11 +0800

    update BucketManager asynFetch, the fileType parameter should be int type, otherwise it will throw request exception.

--- src/main/java/com/qiniu/storage/BucketManager.java
+++ src/main/java/com/qiniu/storage/BucketManager.java
@@ -507,7 +507,7 @@ public final class BucketManager {
      */
     public Response asynFetch(String url, String bucket, String key, String md5, String etag,
                               String callbackurl, String callbackbody, String callbackbodytype,
-                              String callbackhost, String fileType) throws QiniuException {
+                              String callbackhost, int fileType) throws QiniuException {
         String requesturl = configuration.apiHost(auth.accessKey, bucket) + "/sisyphus/fetch";
         StringMap stringMap = new StringMap().put("url", url).put("bucket", bucket).
                 putNotNull("key", key).putNotNull("md5", md5).putNotNull("etag", etag).
