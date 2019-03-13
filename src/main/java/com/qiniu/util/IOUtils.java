f9a354c060c6f10bd63486cdbf6bf067c3fd29ce
缘潘 <peteryuanpan@yuanpandeMacBook-Pro.local>
2019-03-13 14:44:08 +0800

    FormUpload With InputStream

--- src/main/java/com/qiniu/util/IOUtils.java
+++ src/main/java/com/qiniu/util/IOUtils.java
@@ -8,6 +8,13 @@ public class IOUtils {
 	
 	private static final int DEFAULT_BUFFER_SIZE = 1024 * 4;
 
+	/**
+	 * 输入InputSteam，返回byte[].
+	 * 参考：https://github.com/apache/commons-io/blob/master/src/main/java/org/apache/commons/io/IOUtils.java<br>
+	 * @param input
+	 * @return
+	 * @throws IOException
+	 */
     public static byte[] toByteArray(final InputStream input) throws IOException {
         try (final ByteArrayOutputStream output = new ByteArrayOutputStream()) {
             byte[] buffer = new byte[DEFAULT_BUFFER_SIZE];
