69a57c0326ce343cc383c0330be27b9ce9a6dccd
sxci <sxci@foxmail.com>
2019-03-15 10:45:22 +0800

    Revert "添加社会化监控平台相关api."

--- src/main/java/com/qiniu/linking/model/DeviceListing.java
+++
@@ -1,7 +0,0 @@
-package com.qiniu.linking.model;
-
-public class DeviceListing {
-
-    public Device[] items;
-    public String marker;
-}
--- src/main/java/com/qiniu/linking/model/SegmentListing.java
+++
@@ -1,7 +0,0 @@
-package com.qiniu.linking.model;
-
-public class SegmentListing {
-
-    Segment[] items;
-    String marker;
-}
