2fa2b8f3ee5d6e7901759bf13625a69bc5df7d2f
pasocon1993 <pasocon1993@gmail.com>
2019-05-24 10:36:07 +0800

    add more test

--- src/test/java/test/com/qiniu/sms/SmsTest.java
+++ src/test/java/test/com/qiniu/sms/SmsTest.java
@@ -15,6 +15,8 @@ import com.qiniu.common.QiniuException;
 import com.qiniu.http.Client;
 import com.qiniu.http.Response;
 import com.qiniu.sms.SmsManager;
+import com.qiniu.sms.model.SignatureInfo;
+import com.qiniu.sms.model.TemplateInfo;
 import com.qiniu.util.Auth;
 import com.qiniu.util.StringMap;
 
@@ -37,9 +39,109 @@ public class SmsTest {
     @Test
     public void testSendMessage() {
         try {
-        	Map<String,String> paramMap = new HashMap();
-            Response resp = smsManager.sendMessage("test",new String[]{"10086"},paramMap);
-            
+        	Map<String,String> paramMap = new HashMap<String, String>();
+            Response response = smsManager.sendMessage("test",new String[]{"10086"},paramMap);  
+            Assert.assertNotNull(response);
+        } catch (QiniuException e) {
+        	Assert.assertEquals(401, e.code());
+        }
+    }
+    
+    @Test
+    public void testDescribeSignature() {
+    	try {
+            Response response = smsManager.describeSignature("passed",0,0);
+            Assert.assertNotNull(response);
+        } catch (QiniuException e) {
+        	Assert.assertEquals(401, e.code());
+        }
+    }
+    
+    @Test
+    public void testDescribeSignatureItems() {
+    	try {
+            SignatureInfo signatureInfo = smsManager.describeSignatureItems("passed",0,0);
+            Assert.assertNotNull(signatureInfo);
+        } catch (QiniuException e) {
+        	Assert.assertEquals(401, e.code());
+        }
+    }
+    
+    @Test
+    public void testCreateSignature() {
+    	try {
+            Response response = smsManager.createSignature("signature","app",new String[] {"data:image/gif;base64,xxxxxxxxxx"});
+            Assert.assertNotNull(response);
+        } catch (QiniuException e) {
+        	Assert.assertEquals(401, e.code());
+        }
+    }
+    
+    @Test
+    public void testModifySignature() {
+    	try {
+            Response response = smsManager.modifySignature("signatureId","signature");
+            Assert.assertNotNull(response);
+        } catch (QiniuException e) {
+        	Assert.assertEquals(401, e.code());
+        }
+    }
+    
+    @Test
+    public void testDeleteSignature() {
+    	try {
+            Response response = smsManager.deleteSignature("signatureId");
+            Assert.assertNotNull(response);
+        } catch (QiniuException e) {
+        	Assert.assertEquals(401, e.code());
+        }
+    }
+    
+    @Test
+    public void testDescribeTemplate() {
+    	try {
+            Response response = smsManager.describeTemplate("passed",0,0);
+            Assert.assertNotNull(response);
+        } catch (QiniuException e) {
+        	Assert.assertEquals(401, e.code());
+        }
+    }
+    
+    @Test
+    public void testDescribeTemplateItems() {
+    	try {
+    		TemplateInfo templateInfo = smsManager.describeTemplateItems("passed",0,0);
+    		Assert.assertNotNull(templateInfo);
+        } catch (QiniuException e) {
+        	Assert.assertEquals(401, e.code());
+        }
+    }
+    
+    @Test
+    public void testCreateTemplate() {
+    	try {
+            Response response = smsManager.createTemplate("name","template","notification","desc","signatureId");
+            Assert.assertNotNull(response);
+        } catch (QiniuException e) {
+        	Assert.assertEquals(401, e.code());
+        }
+    }
+    
+    @Test
+    public void testModifyTemplate() {
+    	try {
+            Response response = smsManager.modifyTemplate("templateId","name","template","desc","signatureId");
+            Assert.assertNotNull(response);
+        } catch (QiniuException e) {
+        	Assert.assertEquals(401, e.code());
+        }
+    }
+    
+    @Test
+    public void testDeleteTemplate() {
+    	try {
+            Response response = smsManager.deleteTemplate("templateId");
+            Assert.assertNotNull(response);
         } catch (QiniuException e) {
         	Assert.assertEquals(401, e.code());
         }
