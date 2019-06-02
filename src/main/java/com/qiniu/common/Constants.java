5de3fac7abdfa14fb299a519ffab3c6cf1a0c4a0
Peter <peteryuanpan@sina.com>
2019-06-02 14:15:51 +0800

    Update Constants.java

--- src/main/java/com/qiniu/common/Constants.java
+++ src/main/java/com/qiniu/common/Constants.java
@@ -9,7 +9,7 @@ public final class Constants {
     /**
      * 版本号
      */
-    public static final String VERSION = "7.2.18";
+    public static final String VERSION = "7.2.20";
     /**
      * 块大小，不能改变
      */
