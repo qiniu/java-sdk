f7a235ccc5f6e3fa11ba66cefd9e356ad65add23
Sxci <sxci@foxmail.com>
2019-10-10 12:29:30 +0800

    [ci skip] add retry

--- src/main/java/com/qiniu/common/Constants.java
+++ src/main/java/com/qiniu/common/Constants.java
@@ -9,7 +9,7 @@ public final class Constants {
     /**
      * 版本号
      */
-    public static final String VERSION = "7.2.25";
+    public static final String VERSION = "7.2.26";
     /**
      * 块大小，不能改变
      */
