f8b874e7080bf22df2e47cb01a018bbe0eeb845e
zhaolei <zhaolei@qiniu.com>
2020-08-26 14:15:10 +0800

    8.26

--- src/main/java/com/qiniu/qvs/model/StaticLiveRoute.java
+++ src/main/java/com/qiniu/qvs/model/StaticLiveRoute.java
@@ -1,5 +1,9 @@
 package com.qiniu.qvs.model;
 
+import com.qiniu.common.Constants;
+import com.qiniu.util.Md5;
+import com.qiniu.util.UrlSafeBase64;
+
 public class StaticLiveRoute {
 
     private String Domain; // 域名
@@ -39,5 +43,36 @@ public class StaticLiveRoute {
 
     public void setUrlExpireSec(int urlExpireSec) {
         UrlExpireSec = urlExpireSec;
+    }
+
+    public String genStaticHLSFLVDomain(String nsId, String streamId, String key, boolean useHttps) {
+        String path = "/" + nsId + "/" + streamId;
+        String scheme = useHttps ? "https" : "http";
+        String host = "";
+        if ("liveHls".equals(DomainType)) {
+            host = Domain + ":1370";
+            path += ".m3u8";
+        } else {
+            host = Domain + ":1360";
+            path += ".flv";
+        }
+        long expireTime = System.currentTimeMillis() + UrlExpireSec * 1000;
+        String token = signToken(key, path, expireTime);
+        return String.format("%s://%s%s?e=%d&token=%s", scheme, host, path, expireTime, token);
+    }
+
+    public String genStaticRtmpDomain(String nsId, String streamId, String key) {
+        String path = "/" + nsId + "/" + streamId;
+        String scheme = "rtmp";
+        String host = Domain + ":2045";
+        long expireTime = System.currentTimeMillis() + UrlExpireSec * 1000;
+        String token = signToken(key, path, expireTime);
+        return String.format("%s://%s%s?e=%d&token=%s", scheme, host, path, expireTime, token);
+    }
+
+    private String signToken(String key, String path, long expireTime) {
+        String encode_path = UrlSafeBase64.encodeToString(path);
+        String tempS = key + encode_path + Long.toHexString(expireTime);
+        return Md5.md5(tempS.getBytes(Constants.UTF_8));
     }
 }
