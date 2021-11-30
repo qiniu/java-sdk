2bad21ded00e84e9c9d57d4e2fab7a1aa1c73f2b
Hugo <lipeng@qiniu.com>
2021-11-30 15:36:13 +0800

    qvs modify code style

--- src/main/java/com/qiniu/qvs/model/VoiceChat.java
+++ src/main/java/com/qiniu/qvs/model/VoiceChat.java
@@ -1,7 +1,7 @@
 package com.qiniu.qvs.model;
 
 public class VoiceChat {
-    private Boolean isV2;// 该字段为true时，启用低延迟版本，收到返回地址后在发送语音数据
+    private Boolean isV2; // 该字段为true时，启用低延迟版本，收到返回地址后在发送语音数据
     private String[] channels; // 平台设备指定需要启动的通道国标ID（为空表示启动平台下的所有设备）
     private String version; // 对讲国标协议版本，取值"2014"或"2016"，默认为2014，例如大部分大华摄像头为GBT 28181-2014版本对讲模式
     private String transProtocol; // 取值"tcp"或"udp"，流传输模式，默认udp
