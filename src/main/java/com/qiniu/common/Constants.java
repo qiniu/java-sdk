7676cd9f1839e05947b88d369adc7cdbe70305ae
Bai Long <slongbai@gmail.com>
2021-12-30 12:08:26 +0800

    update ver

--- src/main/java/com/qiniu/common/Constants.java
+++ src/main/java/com/qiniu/common/Constants.java
@@ -9,7 +9,7 @@ public final class Constants {
     /**
      * 版本号
      */
-    public static final String VERSION = "7.9.1";
+    public static final String VERSION = "7.9.2";
     /**
      * 块大小，不能改变
      */
