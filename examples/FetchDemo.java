769a0710919468a950da30162c05b09d9dc08786
KING <lllvpipi@gmail.com>
2023-12-23 17:29:19 +0800

    将对过时构造方法new Configuration(Zone z)的调用,修改为Configuration(Region region)

--- examples/FetchDemo.java
+++ examples/FetchDemo.java
@@ -13,8 +13,13 @@ public class FetchDemo {
         String SECRET_KEY = "Secret_Key";
         Auth auth = Auth.create(ACCESS_KEY, SECRET_KEY);
 
-        Zone z = Zone.zone0();
-        Configuration c = new Configuration(z);
+        //第一种方式: 指定具体的要上传的region
+        //指定存储空间所在区域，华北region1，华南region2 ，华东 region0等。具体可以Region类里寻找。
+        //Region region = Region.region1();
+
+        //第一种方式: 使用自动方式
+        Region autoRegion = Region.autoRegion();
+        Configuration c = new Configuration(autoRegion);
 
         //实例化一个BucketManager对象
         BucketManager bucketManager = new BucketManager(auth, c);
