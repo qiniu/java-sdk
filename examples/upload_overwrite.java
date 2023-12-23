e50e7854945cfe905b4dbc5ec8a733c8053b864d
KING <lllvpipi@gmail.com>
2023-12-23 17:06:11 +0800

    1.修改覆盖上传部分注释 2.去掉过时的方法 3.return auth.uploadToken(bucketname, key, 3600);最新版本该构造方法已效.修改为正确的构造方法 4.增加了必要的CDN刷新逻辑 5.try catch 错误信息打印及null point的问题.

--- examples/upload_overwrite.java
+++ examples/upload_overwrite.java
@@ -9,50 +9,38 @@ import com.qiniu.storage.Configuration;
 import java.io.IOException;
 
 public class UploadDemo {
+
     //设置好账号的ACCESS_KEY和SECRET_KEY
     String ACCESS_KEY = "Access_Key";
     String SECRET_KEY = "Secret_Key";
     //要上传的空间
-    String bucketname = "Bucket_Name";
-    //上传到七牛后保存的文件名
-    String key = "my-java.png";
-    //上传文件的路径
-    String filePath = "/.../...";
+    String bucketname = "bucketname";
+    //上传到七牛后保存的路径及文件名
+    String key = "dirName/dirName/test.png";
+    //上传文件的路径[本地文件路径]
+    String filePath = "T:\\download\\chrome\\test.png";
+
+    String bucketname_DOMAIN = "https://www.test.com/";
 
     //密钥配置
     Auth auth = Auth.create(ACCESS_KEY, SECRET_KEY);
 
-    ///////////////////////指定上传的Zone的信息//////////////////
-    //第一种方式: 指定具体的要上传的zone
-    //注：该具体指定的方式和以下自动识别的方式选择其一即可
-    //要上传的空间(bucket)的存储区域为华东时
-    // Zone z = Zone.zone0();
-    //要上传的空间(bucket)的存储区域为华北时
-    // Zone z = Zone.zone1();
-    //要上传的空间(bucket)的存储区域为华南时
-    // Zone z = Zone.zone2();
-
-    //第二种方式: 自动识别要上传的空间(bucket)的存储区域是华东、华北、华南。
-    Zone z = Zone.autoZone();
-    Configuration c = new Configuration(z);
+    Region region = new Region.Builder().autoRegion(Configuration.defaultUcHost);
+    Configuration c = new Configuration(region);
 
     //创建上传对象
     UploadManager uploadManager = new UploadManager(c);
 
-    // 覆盖上传
-    public String getUpToken() {
-        //<bucket>:<key>，表示只允许用户上传指定key的文件。在这种格式下文件默认允许“修改”，已存在同名资源则会被本次覆盖。
-        //如果希望只能上传指定key的文件，并且不允许修改，那么可以将下面的 insertOnly 属性值设为 1。
-        //第三个参数是token的过期时间
-        return auth.uploadToken(bucketname, key, 3600);
-    }
+    //创建CDN管理对象
+    CdnManager cdnManager = new CdnManager(auth);
 
     // 覆盖上传
     public String getUpToken() {
         //<bucket>:<key>，表示只允许用户上传指定key的文件。在这种格式下文件默认允许“修改”，已存在同名资源则会被本次覆盖。
         //如果希望只能上传指定key的文件，并且不允许修改，那么可以将下面的 insertOnly 属性值设为 1。
-        //第三个参数是token的过期时间
-        return auth.uploadToken(bucketname, key, 3600, new StringMap().put("insertOnly", 1));
+        return auth.uploadToken(bucketname, key);
+
+//        return auth.uploadToken(bucketname, key, 3600, new StringMap().put("insertOnly", 1));
     }
 
     public void upload() throws IOException {
@@ -61,17 +49,23 @@ public class UploadDemo {
             Response res = uploadManager.put(filePath, key, getUpToken());
             //打印返回的信息
             System.out.println(res.bodyString());
+
+            String url = bucketname_DOMAIN+key;
+            String[] urls = new String[]{url};
+
+            //如果是覆盖之前旧的，刷新一下CDN，能更快得到新的文件。
+            RefreshResult result = cdnManager.refreshUrls(urls);
+            System.out.println(result.code);
         } catch (QiniuException e) {
+            System.out.println("upload error:"+e.getMessage());
             Response r = e.response;
-            // 请求失败时打印的异常的信息
-            System.out.println(r.toString());
-            try {
-                //响应的文本信息
+            if(r != null && r.bodyString() != null){
                 System.out.println(r.bodyString());
-            } catch (QiniuException e1) {
-                //ignore
             }
         }
     }
 
+    public static void main(String[] args) throws IOException {
+        new QiNiuUtil().upload();
+    }
 }
