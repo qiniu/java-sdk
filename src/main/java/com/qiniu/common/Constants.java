e423a85bdc916baefd5642cdbea02bec25164429
YangSen-qn <yangsen@qiniu.com>
2025-02-05 09:59:09 +0800

    version to 7.18.0

--- src/main/java/com/qiniu/common/Constants.java
+++ src/main/java/com/qiniu/common/Constants.java
@@ -10,7 +10,7 @@ public final class Constants {
     /**
      * 版本号
      */
-    public static final String VERSION = "7.17.0";
+    public static final String VERSION = "7.18.0";
     /**
      * 块大小，不能改变
      */
